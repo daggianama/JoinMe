a64d35d2ec0b83359a3498c16b7de3264f2a7187
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-05-22 19:15:06 +0200

    connecting with database

--- model/database.js
+++ model/database.js
@@ -10,7 +10,7 @@ const con = mysql.createConnection({
   host: DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
   password: DB_PASS,
-  database: DB_NAME || "mvp-data",
+  database: DB_NAME || "mvpData",
   multipleStatements: true
 });
 
@@ -18,10 +18,10 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists items; CREATE TABLE items(id INT NOT NULL AUTO_INCREMENT, text VARCHAR(40) not null, complete BOOLEAN, PRIMARY KEY (id));";
+  let sql = "DROP TABLE if exists items; CREATE TABLE events (id INT AUTO_INCREMENT PRIMARY KEY, eventTitle VARCHAR(255), eventLocation VARCHAR(255), eventDate INT, eventStartTime INT, eventEndTime INT);";
   con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table creation `items` was successful!");
+    console.log("Table creation `events` was successful!");
 
     console.log("Closing...");
   });
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,57 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send({ title: 'Express' });
+router.get('/', sendListBack);
+
+async function sendListBack(req, res, next) {
+  try {
+    const results = await db("SELECT * FROM events ORDER BY eventDate ASC;"); 
+    res.send(results.data);
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+
+  }
+};
+
+router.get("/", (req, res) => {
+  // Send back the full list of items
+  db("SELECT * FROM items ORDER BY id ASC;")
+    .then(results => {
+      res.send(results.data);
+    })
+    .catch(err => res.status(500).send(err));
 });
+
+router.post("/", async (req, res, next) => {
+  const { eventTitle, eventDate, eventStartTime, eventLocation } = req.body;
+  try {
+    await db(
+      `INSERT INTO students (firstname, lastname) VALUES ("${firstname}", "${lastname}");`
+    );
+    // if I reach here, it means the insertion was OK.
+    //here is not necesary to send back the list of students
+    sendListBack(req, res);
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+});
+
+
+// DELETE an event from the database
+router.delete("/:id", async (req, res) => {
+  //your code here
+  const { id } = req.params;
+  try {
+    await db(`DELETE FROM students WHERE id = ${id};`);
+    // If I reach here it means the query was OK.
+     //here is not necessary to send back the list of students
+    // sendListBack(req, res);
+  } catch (err) {
+    res.status(500).send({ message: err });
+  }
+});
+
 
 module.exports = router;
