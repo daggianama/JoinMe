81fc58f7ad34f2fb772a6342c13c51842077ac8f
Daggiana Madrid Andrade <daggianama@gmail.com>
2023-05-22 19:40:23 +0200

    almost main ends points of route index done

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', indexRouter);
+app.use('/api/events', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,81 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const db = require("../model/helper");
 
-/* GET home page. */
-router.get('/', sendListBack);
+
 
 async function sendListBack(req, res, next) {
-  try {
-    const results = await db("SELECT * FROM events ORDER BY eventDate ASC;"); 
-    res.send(results.data);
-  } catch (err) {
-    res.status(500).send({ error: err.message });
+	try {
+		const results = await db(
+			"SELECT * FROM events ORDER BY eventDate ASC;"
+		);
+		res.send(results.data);
+	} catch (err) {
+		res.status(500).send({ error: err.message });
+	}
+}
 
-  }
-};
+// GET all events from the database
+router.get("/", sendListBack);
 
-router.get("/", (req, res) => {
-  // Send back the full list of items
-  db("SELECT * FROM items ORDER BY id ASC;")
-    .then(results => {
-      res.send(results.data);
-    })
-    .catch(err => res.status(500).send(err));
+// GET one event from the database by ID
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	try {
+		const event = await db(`SELECT * FROM events WHERE id = ${id};`);
+		res.send(event.data[0]);
+	} catch (err) {}
 });
 
+// GET one event from the database by date
+router.get("/date", async (req, res) => {
+	const { date } = req.params;
+	try {
+		const event = await db(
+			`SELECT * FROM events WHERE eventDate = ${date};`
+		);
+		res.send(event.data[0]);
+	} catch (err) {}
+});
+
+// GET one event from the database by title
+router.get("/title", async (req, res) => {
+	const { title } = req.params;
+	try {
+		const event = await db(
+			`SELECT * FROM events WHERE eventTitle LIKE '%' + ${title} + '%';`
+		);
+		res.send(event.data[0]);
+	} catch (err) {}
+});
+
+// POST a new event to the database
 router.post("/", async (req, res, next) => {
-  const { eventTitle, eventDate, eventStartTime, eventLocation } = req.body;
-  try {
-    await db(
-      `INSERT INTO students (firstname, lastname) VALUES ("${firstname}", "${lastname}");`
-    );
-    // if I reach here, it means the insertion was OK.
-    //here is not necesary to send back the list of students
-    sendListBack(req, res);
-  } catch (err) {
-    res.status(500).send({ message: err });
-  }
+	const { eventTitle, eventDate, eventStartTime, eventLocation } =
+		req.body;
+	try {
+		await db(
+			`INSERT INTO events (eventTitle, eventDate, eventStartTime, eventLocation) VALUES ("${eventTitle}", "${eventDate}", "${eventStartTime}", "${eventLocation}");`
+		);
+		//here is not necesary to send back any data
+		res
+			.status(201)
+			.send({ message: "Event created successfully :)" });
+	} catch (err) {
+		res.status(500).send({ message: err });
+	}
 });
 
-
-// DELETE an event from the database
+// DELETE an event from the database by ID
 router.delete("/:id", async (req, res) => {
-  //your code here
-  const { id } = req.params;
-  try {
-    await db(`DELETE FROM students WHERE id = ${id};`);
-    // If I reach here it means the query was OK.
-     //here is not necessary to send back the list of students
-    // sendListBack(req, res);
-  } catch (err) {
-    res.status(500).send({ message: err });
-  }
+	const { id } = req.params;
+	try {
+		await db(`DELETE FROM events WHERE id = ${id};`);
+		//here is not necessary to send back nothing
+		res.status(201).send({ message: "Event deleted successfully" });
+	} catch (err) {
+		res.status(500).send({ message: err });
+	}
 });
-
 
 module.exports = router;
